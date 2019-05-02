a11f69aa5daa78b0f7d791929076a05caa8cf664
krskelton <kskelton82@gmail.com>
2019-05-02 10:41:40 -0600

    Update get_country_codes.py

--- get_country_codes.py
+++ get_country_codes.py
@@ -1,3 +1,12 @@
 def get_country_codes(param):
-    # Your code here. Maybe try pseudocoding this one out?
-    return ""
+    # your code here
+    words = param.split()
+    country_codes = []
+    for word in words:
+        new_word = ''
+        for char in word:
+            if char.isalpha():
+                new_word += char
+        country_codes.append(new_word)
+        return_string = ", ".join(country_codes)
+    return return_string
